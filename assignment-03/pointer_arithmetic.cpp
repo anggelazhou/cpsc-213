162f069669669f2f2897d0ff308cf3dd1e75722a
UBC Student <anggelaz@valdes.students.cs.ubc.ca>
2021-02-06 19:38:41 -0800

    Finish bubble_sort_awesome and pointer_arithmetic

---
+++ assignment-03/pointer_arithmetic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10] = { 45, 45, 13, 40, 19, 18, 32, 37, 26, 38 };
+    cout << "a=" << a << "*a" << (*a) << endl;
+    cout << *(a + 9) << endl;
+    cout << &a[1] - &a[6] << endl;
+    cout << *(a + (&a[1] - a + 6)) << endl;
+    return 0;
+}
