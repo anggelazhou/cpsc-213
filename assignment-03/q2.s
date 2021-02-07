d26821c2218c6f6ec4f7d9fc035e5fcc2863298c
Angela Zhou <angelazhouqi@gmail.com>
2021-02-06 21:44:35 -0500

    Finish q2

--- assignment-03/q2.s
+++ assignment-03/q2.s
@@ -12,44 +12,47 @@
         ld (r0), r5         # r5 = tos
         st r3, (r4, r5, 4)  # s[tos] = a[0]
 
-        inc r5              # tos++
+        inc r5              # r5++
+        st r5, (r0)         # tos++ 
         
         ld $1, r1           # r1 = 1 
         ld (r2, r1, 4), r3  # r3 = a[1]
         ld $s, r4           # r4 = address of s
         st r3, (r4, r5, 4)  # s[tos] = a[1]
 
-        inc r5              # tos++
+        inc r5              # r5++
+        st r5, (r0)         # tos++
         
         ld $2, r1           # r1 = 2 
         ld (r2, r1, 4), r3  # r3 = a[2]
         ld $s, r4           # r4 = address of s
         st r3, (r4, r5, 4)  # s[tos] = a[2]
 
-        inc r5              # tos++
+        inc r5              # r5++
+        st r5, (r0)         # tos++
 
-        dec r5              # tos--
-
-        ld (r4, r5, 4), r3  # r3 = s[tos]
-        ld $tmp, r6         # r6 = address of tmp
-        ld r6, r7           # r7 = tmp
-        st r3, (r7)         # tmp = s[tos]
-
-        dec r5              # tos--
+        dec r5              # r5--
+        st r5, (r0)         # tos--
 
         ld (r4, r5, 4), r3  # r3 = s[tos]
         ld $tmp, r6         # r6 = address of tmp
-        ld (r6), r7         # r7 = tmp
-        add r3, r7          # r7 = tmp + s[tos]
-        st r7, (r6)         # tmp = tmp + s[tos]
+        st r3, (r6)         # tmp = s[tos]
 
-        dec r5              # tos--
+        dec r5              # r5--
+        st r5, (r0)         # tos--
 
         ld (r4, r5, 4), r3  # r3 = s[tos]
-        ld $tmp, r6         # r6 = address of tmp
-        ld 0(r6), r7        # r7 = tmp
-        add r3, r7          # r7 = tmp + s[tos]
-        st r7, (r6)         # tmp = tmp + s[tos]
+        ld (r6), r7         # r7 = value of tmp
+        add r3, r7          # tmp = tmp + s[tos]
+        st r7, (r6)         # store in memory address 
+
+        dec r5              # r5--
+        st r5, (r0)         # tos--
+
+        ld (r4, r5, 4), r3  # r3 = s[tos]
+        ld (r6), r7         # r7 = value of tmp
+        add r3, r7          # tmp = tmp + s[tos]
+        st r7, (r6)         # store in memory address 
 
         halt                # halt
 
@@ -64,6 +67,6 @@ s: .long 0x00000011
    .long 0x00000014
    .long 0x00000015
 .pos 0x4000
-a: .long 0x00000016
-   .long 0x00000017
-   .long 0x00000018
+a: .long 0x00000001
+   .long 0x00000002
+   .long 0x00000003
