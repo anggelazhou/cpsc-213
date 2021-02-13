ed32f53999732248fd54e28b2d035acb62bed9d1
UBC Student <anggelaz@valdes.students.cs.ubc.ca>
2021-02-13 14:56:20 -0800

    Fix strcpy
    Use strlen instead of sizeof

--- assignment-03/pointer_arithmetic.cpp
+++ assignment-03/pointer_arithmetic.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     int a[10] = { 45, 45, 13, 40, 19, 18, 32, 37, 26, 38 };
     cout << "a=" << a << "*a" << (*a) << endl;
+    cout << &a[1] << endl;
+    cout << &a[6] << endl;
+    cout << "---------------------------" << endl;
+    cout << *(a + 0) << endl;
     cout << *(a + 9) << endl;
     cout << &a[1] - &a[6] << endl;
     cout << *(a + (&a[1] - a + 6)) << endl;
