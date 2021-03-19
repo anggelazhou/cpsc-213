3b97ab52840919c8d25c1872f51ba02d458c69d3
Angela Zhou <angelazhouqi@gmail.com>
2021-03-19 01:58:49 -0400

    First draft of a7.7

--- assignment-07/q3/int_element.c
+++ assignment-07/q3/int_element.c
@@ -1,5 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "int_element.h"
+#include "refcount.h"
 
 /* TODO: Implement all public int_element functions, including element interface functions.
 
 You may add your own private functions here too. */
+
+/* Forward reference to a int_element. You get to define the structure. */
+struct int_element_class {
+  void (*print)(struct int_element *);
+  void (*compare)(struct int_element *, struct int_element *);
+  int type;
+};
+
+struct int_element {
+  struct int_element_class *class;
+  int    i;
+};
+
+void int_element_print(struct int_element * i_element) {
+  printf("%d", i_element->i);
+}
+
+int int_element_compare(struct int_element * i_e1, struct int_element * i_e2) {
+  return i_e1->i - i_e2->i;
+}
+
+struct int_element_class  int_element_class= {int_element_print, int_element_compare, 0};
+
+void int_element_finalizer(void * p) {
+   // struct int_element * i_e = p;
+    // free(i_e);
+}
+
+/* Static constructor that creates new integer elements. */
+struct int_element *int_element_new(int value) {
+    struct int_element *obj = rc_malloc(sizeof(struct int_element), int_element_finalizer);
+    obj->class = &int_element_class;
+    obj->i     = value;
+    return obj;
+}
+
+/* Static function that obtains the value held in an int_element. */
+int int_element_get_value(struct int_element * i_element) {
+    return i_element->i;
+}
+
+/* Static function that determines whether this is an int_element. */
+int is_int_element(struct element * element) {
+    return ((struct int_element *)element)->class->type == 0;
+}
--- assignment-07/q3/str_element.c
+++ assignment-07/q3/str_element.c
@@ -1,5 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "str_element.h"
+#include "refcount.h"
 
 /* TODO: Implement all public str_element functions, including element interface functions.
 
 You may add your own private functions here too. */
+
+/* Forward reference to a str_element. You get to define the structure. */
+struct str_element_class {
+  void (*print)(struct str_element *);
+  void (*compare)(struct str_element *, struct str_element *);
+  int type;
+};
+
+struct str_element {
+  struct str_element_class *class;
+  char    *c;
+};
+
+void str_element_print(struct str_element * s_element) {
+  printf("%s", s_element->c);
+}
+
+int str_element_compare(struct str_element * s_e1, struct str_element * s_e2) {
+  int i = 0;
+  while (1) {
+      char c1 = s_e1->c[i];
+      char c2 = s_e2->c[i];
+
+      if (c1 == '\0') {
+          return c2 == '\0' ? 0 : -1;
+      } else if (c2 == '\0') {
+          return 1;
+      } else {
+          int k = (int) c1 - (int) c2;
+          if (k != 0) {
+              return k;
+          } else {
+              i++;
+          }
+      }
+  }
+}
+
+struct str_element_class  str_element_class= {str_element_print, str_element_compare, 1};
+
+void str_element_finalizer(void * p) {
+    struct str_element * s_e = p;
+    free(s_e->c);
+}
+
+/* Static constructor that creates new string elements. */
+struct str_element *str_element_new(char *value) {
+    struct str_element *obj = rc_malloc(sizeof(struct str_element), str_element_finalizer);
+    obj->class = &str_element_class;
+    obj->c     = malloc(sizeof(value)+1);
+    for ( char *p = obj->c; ( *p = *value ) != '\0'; ++p, ++value );
+    rc_keep_ref(obj->c);
+    return obj;
+}
+
+/* Static function that obtains the string from a str_element. The caller should keep_ref it. */
+char *str_element_get_value(struct str_element * s_element) {
+    return s_element->c;
+}
+
+/* Static function that determines whether this is a str_element. */
+int is_str_element(struct element * element) {
+    return ((struct str_element *)element)->class->type == 1;
+}
